c1f91ab7c9e0cd6bc8bc604f7154c90a1a455fef
Nathan Muir <ndmuir@gmail.com>
2015-01-23 17:14:34 +1000

    Initial commit for public release.

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: '3stack:embox-value',
+  version: '0.1.0',
+  summary: 'A tool to cache & recompute the value of a reactive computation',
+  git: 'https://github.com/3stack-software/meteor-embox-value',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api){
+  api.versionsFrom('METEOR@0.9.2');
+
+  api.export('emboxValue', 'client');
+
+  api.use([
+    'tracker'
+  ], 'client');
+  api.use('blaze', {weak: true});
+  api.addFiles([
+    'embox-value.js',
+    'template-hooks.js'
+  ], 'client');
+
+});
