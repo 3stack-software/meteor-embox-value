5654903526f8c94b1a41bae2cc411adc7e5f16ce
Richard Lai <it.rclai@gmail.com>
2015-04-30 23:43:30 -0400

    Define self

--- template-hooks.js
+++ template-hooks.js
@@ -1,16 +1,17 @@
 
 // Hooks embox's in to template hooks automatically
-if (Package.blaze){
+if (Package.blaze) {
   var Blaze = Package.blaze.Blaze;
-  Blaze.Template.prototype.emboxValue = function(f, equals){
+  Blaze.Template.prototype.emboxValue = function (f, equals) {
     return this.view.emboxValue(f, equals);
   };
-  Blaze.View.prototype.emboxValue = function(f, equals){
+  Blaze.View.prototype.emboxValue = function (f, equals) {
+    var self = this;
     // from Blaze.View.prototype.autorun
     if (! self.isCreated) {
       throw new Error("View#autorun must be called from the created callback at the earliest");
     }
-    if (this._isInRender) {
+    if (self._isInRender) {
       throw new Error("Can't call View#autorun from inside render(); try calling it from the created or rendered callback");
     }
     if (Tracker.active) {
